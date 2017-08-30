70d8a34fa065452bac2c0a1806f68420bc51be57
jukkak <jukkak@siililinux.localdomain>
2017-08-30 12:29:06 +0300

    initial commit

---
+++ pythia-backend/src/main/java/fi/espoo/pythia/PythiaBackendApplication.java
@@ -0,0 +1,12 @@
+package fi.espoo.pythia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PythiaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PythiaBackendApplication.class, args);
+	}
+}
---
+++ pythia-backend/src/test/java/fi/espoo/pythia/PythiaBackendApplicationTests.java
@@ -0,0 +1,16 @@
+package fi.espoo.pythia;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PythiaBackendApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
