f400482571a23ded5afd961e6b8e965495eeb3e7
Jukka Koskimäki <jukka.koskimaki@siili.com>
2017-08-31 19:06:42 +0300

    some more

--- pythia-backend/src/main/java/fi/espoo/pythia/backend/ApplicationConfig.java
+++ pythia-backend/src/main/java/fi/espoo/pythia/backend/ApplicationConfig.java
@@ -7,13 +7,13 @@ import javax.sql.DataSource;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
-import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 /**
@@ -98,11 +98,5 @@ public class ApplicationConfig {
 	        transactionManager.setEntityManagerFactory(entityManagerFactory);
 	        return transactionManager;
 	    }
-	  @Bean
-	  public PlatformTransactionManager transactionManager() {
-
-	    JpaTransactionManager txManager = new JpaTransactionManager();
-	    txManager.setEntityManagerFactory(entityManagerFactory());
-	    return txManager;
-	  }
+	   
 }
