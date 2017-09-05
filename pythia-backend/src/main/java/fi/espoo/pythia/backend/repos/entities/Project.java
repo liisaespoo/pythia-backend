4a01f673758f7107f1431386ef846fa83351506e
Jukka Koskimäki <jukka.koskimaki@siili.com>
2017-09-05 15:38:21 +0300

    copy-paste how to do the sequence generator

--- pythia-backend/src/main/java/fi/espoo/pythia/backend/repos/entities/Project.java
+++ pythia-backend/src/main/java/fi/espoo/pythia/backend/repos/entities/Project.java
@@ -1,5 +1,16 @@
 package fi.espoo.pythia.backend.repos.entities;
 
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+
 public class Project {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "book_generator")
+	@SequenceGenerator(name="book_generator", sequenceName = "book_seq", allocationSize=50)
+	@Column(name = "id", updatable = false, nullable = false)
+	 private Long id;
 }
