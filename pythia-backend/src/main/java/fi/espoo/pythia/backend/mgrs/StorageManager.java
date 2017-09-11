bd2965921b54439cec6b9c6cf63d2024b12cf9dc
tapir79 <saara.m.pakarinen@gmail.com>
2017-09-11 16:09:40 +0300

    projectvalue and mapping

--- pythia-backend/src/main/java/fi/espoo/pythia/backend/mgrs/StorageManager.java
+++ pythia-backend/src/main/java/fi/espoo/pythia/backend/mgrs/StorageManager.java
@@ -8,9 +8,12 @@ import javax.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import fi.espoo.pythia.backend.mappers.ProjectToProjectValue;
+import fi.espoo.pythia.backend.mappers.ProjectValueToProject;
 import fi.espoo.pythia.backend.repos.PlanRepository;
 import fi.espoo.pythia.backend.repos.ProjectRepository;
 import fi.espoo.pythia.backend.repos.entities.Project;
+import fi.espoo.pythia.backend.transfer.ProjectValue;
 
 @Component
 @Transactional
@@ -25,76 +28,102 @@ public class StorageManager {
 	
 
 	/**
-	 * Create new project in database.
+	 * Create new project in database. DONE!!!!
 	 * 
 	 */
-	public Project createProject(Project project) {
+	public ProjectValue createProject(ProjectValue projectV) {
 
-		// timestamp with time at db or microservice level
-		project.setCreatedAt(null);
+		//map projectV to project
+		ProjectValueToProject pv2pmapping = new ProjectValueToProject();
+		Project prj = pv2pmapping.getProject(projectV);
 		
-		Project savedProject = projectRepository.save(project);
+		// timestamp with time at db or microservice level
+		prj.setCreatedAt(null);
+		Project savedProject = projectRepository.save(prj);
 
 		// planRepository.save(5L);
 
 		// planRepository.
-		return project;
+		return projectV;
 
 	}
-
+	
 	/**
-	 * Returns list of projects from database.
+	 * Returns list of projects from database. DONE!!!
 	 * 
 	 * @return list of projects
 	 */
-	public ArrayList<Project> getProjects() {
+	public ArrayList<ProjectValue> getProjects() {
 		
-		return (ArrayList<Project>) projectRepository.findAll();
-
+		ArrayList<Project> prjList = (ArrayList<Project>) projectRepository.findAll();
+		ArrayList<ProjectValue> prjValList = new ArrayList();
+		
+		// for -loop for prjList
+		ProjectToProjectValue p2pvmapping = new ProjectToProjectValue();
+		
+		for (Project p : prjList) {
+			
+			// map each project to projectValue 
+			ProjectValue pval = p2pvmapping.getProjectValue(p);
+			prjValList.add(pval);
+		}
+		
+		// return projectValue -ArrayList 
+		
+		return prjValList;
 	}
+	
+	
 
 	/**
 	 * Return project object for given id from database. If project is not found for
-	 * id, returns null.
+	 * id, returns null. DONE
 	 */
-	public Project getProject(Long projectId) {
+	public ProjectValue getProject(Long projectId) {
 		// TODO Auto-generated method stub
+		ProjectToProjectValue p2pvmapping = new ProjectToProjectValue();
 
-		List<Project> projects = getProjects();
-		for (Project p : projects) {
-			if (p.getProjectId().equals(projectId)) {
+		List<ProjectValue> projects = getProjects();
+		for (ProjectValue p : projects) {
+			if (p.getProjectId().equals(projectId)) {				
 				return p;
 			}
 		}
 		return null;
 	}
 	
+
+	
 	
 	/**
 	 * 
 	 * Update the project object for given id in database. If project does
-	 * not exists, return null
+	 * not exists, return null DONE!!!
 	 * 
 	 * @param projectId
 	 * @param project
 	 * @return
 	 */
-	public Project update(Long projectId, Project project) {
+	public ProjectValue update(Long projectId, ProjectValue projectV) {
 		
-		List<Project> projects = getProjects();
+		List<ProjectValue> projects = getProjects();
 		
-		// find project object with id
-		for (Project p : projects) {
+		// find projectvalue object with id
+		for (ProjectValue p : projects) {
 			
 			if (p.getProjectId().equals(projectId)) {
+				
+				// map projectvalue to project
+				ProjectValueToProject pv2pmapping = new ProjectValueToProject();
+				Project prj = pv2pmapping.getProject(projectV);
 				
 				// remove current entity with projectId
 				projectRepository.delete(projectId);
 				
 				// add new entity
-				projectRepository.save(project);
+				projectRepository.save(prj);
 				
-				return project;
+				return projectV;
 			}
 		}
 
