a647750045ec8ba044e5a4f48afa49423b1750cd
tapir79 <saara.m.pakarinen@gmail.com>
2017-09-12 12:07:26 +0300

    static methods

---
+++ pythia-backend/src/main/java/fi/espoo/pythia/backend/mappers/ProjectToProjectValueMapper.java
@@ -0,0 +1,27 @@
+package fi.espoo.pythia.backend.mappers;
+
+import java.time.OffsetDateTime;
+
+import fi.espoo.pythia.backend.repos.entities.Project;
+import fi.espoo.pythia.backend.transfer.ProjectValue;
+
+public class ProjectToProjectValueMapper {
+
+	
+	public static ProjectValue projectToProjectValue(Project p) {
+		
+		ProjectValue pv = new ProjectValue();
+		pv.setProjectId(p.getProjectId());
+		pv.setHansuProjectId(p.getHansuProjectId());
+		pv.setName(p.getName());
+		pv.setDescription(p.getDescription());
+//		pv.setCreatedAt(p.getCreatedAt());
+		pv.setCreatedBy(p.getCreatedBy());
+//		pv.setUpdatedAt(p.getUpdatedAt());
+		pv.setUpdatedBy(p.getUpdatedBy());
+		
+		return pv;
+	}
+	
+
+}
