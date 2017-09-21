7a3b18348b4d056caa1469870e97c16d06b6acaf
tapir79 <saara.m.pakarinen@gmail.com>
2017-09-21 11:41:35 +0300

    Make mapper planvalue to plan

--- pythia-backend/src/main/java/fi/espoo/pythia/backend/mappers/PlanValueToPlanMapper.java
+++ pythia-backend/src/main/java/fi/espoo/pythia/backend/mappers/PlanValueToPlanMapper.java
@@ -1,5 +1,34 @@
 package fi.espoo.pythia.backend.mappers;
 
+import fi.espoo.pythia.backend.repos.ProjectRepository;
+import fi.espoo.pythia.backend.repos.entities.Plan;
+import fi.espoo.pythia.backend.repos.entities.Project;
+import fi.espoo.pythia.backend.transfer.PlanValue;
+
 public class PlanValueToPlanMapper {
 
+	
+	public static Plan planValueToPlan(PlanValue pv, ProjectRepository pr) {
+		
+		//get project_id 
+		Long projectId = pv.getProjectId();
+		//get project by projectid
+		Project project = pr.findByProjectId(projectId);
+
+		Plan p = new Plan();
+		
+		p.setPlanId(pv.getPlanId());
+		p.setProject(project);
+		p.setMainNo(pv.getMainNo());
+		p.setSubNo(pv.getSubNo());
+		p.setVersion(pv.getVersion());
+				
+//		p.setCreatedAt(pv.getCreatedAt());
+//		p.setCreatedBy(pv.getCreatedBy());
+//		p.setUpdatedAt(pv.getUpdatedAt());
+//		p.setUpdatedBy(pv.getUpdatedBy());
+		
+		return p;
+	}
+	
 }
--- pythia-backend/src/main/java/fi/espoo/pythia/backend/transfer/PlanValue.java
+++ pythia-backend/src/main/java/fi/espoo/pythia/backend/transfer/PlanValue.java
@@ -2,6 +2,78 @@ package fi.espoo.pythia.backend.transfer;
 
 import java.io.Serializable;
 
+import javax.persistence.Column;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import fi.espoo.pythia.backend.repos.entities.Project;
+
 public class PlanValue  implements Serializable {
 
+	private Long planId;
+
+	
+	private Long projectId;
+
+
+	private short mainNo;
+
+
+	private short subNo;
+
+
+	private String version;
+
+
+	public Long getPlanId() {
+		return planId;
+	}
+
+
+	public void setPlanId(Long planId) {
+		this.planId = planId;
+	}
+
+
+	public Long getProjectId() {
+		return projectId;
+	}
+
+
+	public void setProjectId(Long projectId) {
+		this.projectId = projectId;
+	}
+
+
+	public short getMainNo() {
+		return mainNo;
+	}
+
+
+	public void setMainNo(short mainNo) {
+		this.mainNo = mainNo;
+	}
+
+
+	public short getSubNo() {
+		return subNo;
+	}
+
+
+	public void setSubNo(short subNo) {
+		this.subNo = subNo;
+	}
+
+
+	public String getVersion() {
+		return version;
+	}
+
+
+	public void setVersion(String version) {
+		this.version = version;
+	}
+	
+	
+	
 }
