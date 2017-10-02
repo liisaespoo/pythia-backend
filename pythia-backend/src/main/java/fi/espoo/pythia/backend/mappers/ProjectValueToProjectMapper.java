01fdbdf01757ee0e26b895f5372d7684f572702d
pakars4 <pakars4@LTH001104.espoo.ad.city>
2017-10-02 23:12:44 +0300

    Add ProjectMapping entity ProjectMappingValue and mappers

--- pythia-backend/src/main/java/fi/espoo/pythia/backend/mappers/ProjectValueToProjectMapper.java
+++ pythia-backend/src/main/java/fi/espoo/pythia/backend/mappers/ProjectValueToProjectMapper.java
@@ -6,15 +6,16 @@ import java.util.List;
 
 import fi.espoo.pythia.backend.repos.entities.Plan;
 import fi.espoo.pythia.backend.repos.entities.Project;
+import fi.espoo.pythia.backend.repos.entities.ProjectMapping;
 import fi.espoo.pythia.backend.transfer.PlanValue;
+import fi.espoo.pythia.backend.transfer.ProjectMappingValue;
 import fi.espoo.pythia.backend.transfer.ProjectValue;
 
 public class ProjectValueToProjectMapper {
-	
-	
-	//new project
-	public static Project projectValueToProject(ProjectValue pv) {
-		
+
+	// new project
+	public static Project projectValueToProject(ProjectValue pv, Project project) {
+
 		Project p = new Project();
 		p.setProjectId(pv.getProjectId());
 		p.setHansuProjectId(pv.getHansuProjectId());
@@ -22,35 +23,55 @@ public class ProjectValueToProjectMapper {
 		p.setName(pv.getName());
 		p.setDescription(pv.getDescription());
 		p.setPlans(new ArrayList<Plan>());
-	
-//		p.setCreatedAt(pv.getCreatedAt());
-//		p.setCreatedBy(pv.getCreatedBy());
-//		p.setUpdatedAt(pv.getUpdatedAt());
-//		p.setUpdatedBy(pv.getUpdatedBy());
-		
+
+		List<ProjectMapping> sisterids = new ArrayList();
+		for (ProjectMappingValue pmv : pv.getSisterProjects()) {
+			ProjectMapping pm = new ProjectMapping();
+			pm.setMappingId(pmv.getMappingId());
+			pm.setProject(project);
+			pm.setSisterProjectId(pmv.getSisterProjectId());
+			sisterids.add(pm);
+		}
+		p.setSisterProjects(sisterids);
+
+		// p.setCreatedAt(pv.getCreatedAt());
+		// p.setCreatedBy(pv.getCreatedBy());
+		// p.setUpdatedAt(pv.getUpdatedAt());
+		// p.setUpdatedBy(pv.getUpdatedBy());
+
 		return p;
 	}
-	
-	//updating a project
-	public static Project projectValueToProjectUpdate(ProjectValue pv){
+
+	// updating a project
+	public static Project projectValueToProjectUpdate(ProjectValue pv, Project project) {
 		Project p = new Project();
 		p.setProjectId(pv.getProjectId());
 		p.setHansuProjectId(pv.getHansuProjectId());
 		p.setMainNo(pv.getMainNo());
 		p.setName(pv.getName());
 		p.setDescription(pv.getDescription());
-		
+
 		List<Plan> plans = new ArrayList();
-		for(PlanValue pp : pv.getPlans()){
+		for (PlanValue pp : pv.getPlans()) {
 			plans.add(PlanValueToPlanMapper.planValueToPlan(pp, p));
 		}
 		p.setPlans(plans);
-	
-//		p.setCreatedAt(pv.getCreatedAt());
-//		p.setCreatedBy(pv.getCreatedBy());
-//		p.setUpdatedAt(pv.getUpdatedAt());
-//		p.setUpdatedBy(pv.getUpdatedBy());
-		
+
+		List<ProjectMapping> sisterids = new ArrayList();
+		for (ProjectMappingValue pmv : pv.getSisterProjects()) {
+			ProjectMapping pm = new ProjectMapping();
+			pm.setMappingId(pmv.getMappingId());
+			pm.setProject(project);
+			pm.setSisterProjectId(pmv.getSisterProjectId());
+			sisterids.add(pm);
+		}
+		p.setSisterProjects(sisterids);
+
+		// p.setCreatedAt(pv.getCreatedAt());
+		// p.setCreatedBy(pv.getCreatedBy());
+		// p.setUpdatedAt(pv.getUpdatedAt());
+		// p.setUpdatedBy(pv.getUpdatedBy());
+
 		return p;
 	}
 
