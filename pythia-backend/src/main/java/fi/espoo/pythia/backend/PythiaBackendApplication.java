49f90993ce9c0635cc8eca865cd294fc23202b1a
pakars4 <pakars4@LTH001104.espoo.ad.city>
2017-10-13 11:16:15 +0300

    Add file post size tomcat

--- pythia-backend/src/main/java/fi/espoo/pythia/backend/PythiaBackendApplication.java
+++ pythia-backend/src/main/java/fi/espoo/pythia/backend/PythiaBackendApplication.java
@@ -1,12 +1,54 @@
 package fi.espoo.pythia.backend;
 
+import org.apache.coyote.http11.AbstractHttp11Protocol;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.boot.context.embedded.tomcat.TomcatConnectorCustomizer;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class PythiaBackendApplication {
 
+	private int maxUploadSizeInMb = 10 * 1024 * 1024; // 10 MB
+	
 	public static void main(String[] args) {
 		SpringApplication.run(PythiaBackendApplication.class, args);
 	}
+	
+//	 //Tomcat large file upload connection reset
+//    //http://www.mkyong.com/spring/spring-file-upload-and-connection-reset-issue/
+//    @Bean
+//    public TomcatEmbeddedServletContainerFactory tomcatEmbedded() {
+//
+//        TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory();
+//
+//        tomcat.addConnectorCustomizers((TomcatConnectorCustomizer) connector -> {
+//            if ((connector.getProtocolHandler() instanceof AbstractHttp11Protocol<?>)) {
+//                //-1 means unlimited
+//                ((AbstractHttp11Protocol<?>) connector.getProtocolHandler()).setMaxSwallowSize(-1);
+//            }
+//        });
+//
+//        return tomcat;
+//
+//    }
+    
+    
+    @Bean
+    public EmbeddedServletContainerCustomizer containerCustomizer() throws Exception {
+        return (ConfigurableEmbeddedServletContainer container) -> {
+            if (container instanceof TomcatEmbeddedServletContainerFactory) {
+                TomcatEmbeddedServletContainerFactory tomcat = (TomcatEmbeddedServletContainerFactory) container;
+                tomcat.addConnectorCustomizers(
+                    (connector) -> {
+                        connector.setMaxPostSize(100000000); // 100 MB
+                    }
+                );
+            }
+        };
+    }
+
 }
